d8a1007df998bdab934359c39574f677c1113dbd
Eric Lee <ericleecodes@gmail.com>
2024-03-27 23:36:47 -0700

    Save current progress

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setHistory(prev => [...prev, newMode]);
+  }
+
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+};
+
